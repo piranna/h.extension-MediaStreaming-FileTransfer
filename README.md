d20da02faab813d3a7917ccb09f109acf3c72ae2
Agustín Hernández Pérez <agustin.hernandez@atos.net>
2020-07-08 08:56:22 +0100

    Release 1.0.0

---
+++ README.md
@@ -0,0 +1,92 @@
+# MediaStreaming - FileTransfer
+
+Repository for the FileTransfer of Media Streaming Module (T3.3).
+
+### File Transfer:
+Thanks to the TUS server, a user can upload content from the mobile phone to the personal storage. The upload can be paused and resumed if needed. The default configuration is:
+
+* TURN server url: https://builder.helios-social.eu/files/
+
+All these values can be modified in the `values/strings.xml` file.
+
+### How to use File Transfer:
+```
+        Intent fileTransferIntent = new Intent(MainActivity.this, FileTransferActivity.class);
+        MainActivity.this.startActivity(fileTransferIntent);
+```
+
+### Request permissions
+There are non additional permissions needed.
+
+## Multiproject dependencies ##
+
+HELIOS software components are organized into different repositories
+so that these components can be developed separately avoiding many
+conflicts in code integration. However, the modules also depend on
+each other.
+
+### How to configure the dependencies ###
+
+To manage project dependencies developed by the consortium, the approach proposed is to use a private Maven repository with Nexus.
+
+To avoid clone all dependencies projects in local, to compile the "father" project. Otherwise, a developer should have all the projects locally to
+be able to compile. Using Nexus, the dependencies are located in a remote repository, available to compile, as described in the next section.
+Also to improve the automation for deploy, versioning and distribution of the project.
+
+### How to use the HELIOS Nexus ###
+
+Similar to other dependencies available in Maven Central, Google or others repositories. In this case we specify the Nexus
+repository provided by Atos: `https://builder.helios-social.eu/repository/helios-repository/`
+
+This URL makes the project dependencies available.
+
+To access, we simply need credentials, that we will define locally in the variables `heliosUser` and `heliosPassword`.
+
+The `build.gradle` of the project define the Nexus repository and the credential variables in this way:
+
+```
+repositories {
+        ...
+        maven {
+            url "https://builder.helios-social.eu/repository/helios-repository/"
+            credentials {
+                username = heliosUser
+                password = heliosPassword
+            }
+        }
+    }
+```
+
+And the variables of Nexus's credentials are stored locally at `~/.gradle/gradle.properties`:
+
+```
+heliosUser=username
+heliosPassword=password
+```
+
+To request Nexus username and password, contact with: `carlosalberto.martinedo@atos.net`
+
+### How to deploy a new version of the dependencies ###
+
+Let's say that we want to deploy a new version of the videocall project. This project is a dependency of MediaStreaming.
+For Continuous Integration we use Jenkins. It deploys the configured projects (e.g., videocall) in different jobs,
+and the results are libraries packaged like AAR (Android ARchive). These packaged libraries are upload to Nexus and in this way,
+they are available to build the projects that depend on them (e.g., MediaStreaming).
+In the videocall example, Jenkins jobs generate automatically and aar library and store it at the Nexus repository to make it available.
+
+Jenkins is the tool deployed by Atos (WP6 leader) in HELIOS to automate the generation of APKs, joining all the project modules.
+Due to the need of managing the dependencies, Atos has selected additional tools, as explained in this document.
+
+After pushing a change to the `master` branch, the maintainer can builds the module by means of the job in the Jenkins interface. GitLab repositories are set to protect
+the `master` branch push and merge for the partner in charge of its module/project (maintainer).
+
+To request Jenkins username and password, contact with: `carlosalberto.martinedo@atos.net`
+
+### How to use the dependencies ###
+
+To use the dependency in `build.gradle` of the "father" project, you should specify the last version available in Nexus, related to the last Jenkins's deploy.
+For example, to declare the dependency on the filetransfer module and the respective version:
+
+`implementation 'eu.h2020.helios_social.modules.filetransfer:filetransfer:1.0.5'`
+
+For more info review: `https://scm.atosresearch.eu/ari/helios_group/generic-issues/blob/master/multiprojectDependencies.md`
---
+++ app/src/main/java/eu/h2020/helios_social/modules/filetransfer/FileTransferActivity.java
@@ -0,0 +1,226 @@
+package eu.h2020.helios_social.modules.filetransfer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.net.URL;
+
+import io.tus.android.client.TusAndroidUpload;
+import io.tus.android.client.TusPreferencesURLStore;
+import io.tus.java.client.TusClient;
+import io.tus.java.client.TusUpload;
+import io.tus.java.client.TusUploader;
+
+
+public class FileTransferActivity extends AppCompatActivity {
+    private final int REQUEST_FILE_SELECT = 1;
+    private TusClient client;
+    private TextView status;
+    private Button pauseButton;
+    private Button resumeButton;
+    private UploadTask uploadTask;
+    private ProgressBar progressBar;
+    private Uri fileUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_transfer);
+
+        try {
+            SharedPreferences pref = getSharedPreferences("tus", 0);
+            client = new TusClient();
+            String url = getString(R.string.TUS_URL);
+            client.setUploadCreationURL(new URL(url));
+            client.enableResuming(new TusPreferencesURLStore(pref));
+        } catch(Exception e) {
+            showError(e);
+        }
+
+        status = findViewById(R.id.status);
+        progressBar = findViewById(R.id.progressBar);
+
+        Button button = findViewById(R.id.upload_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("*/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select file to upload"), REQUEST_FILE_SELECT);
+            }
+        });
+
+        pauseButton = findViewById(R.id.pause_button);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pauseUpload();
+            }
+        });
+
+        resumeButton = findViewById(R.id.resume_button);
+        resumeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resumeUpload();
+            }
+        });
+    }
+
+    private void beginUpload(Uri uri) {
+        fileUri = uri;
+        resumeUpload();
+    }
+
+    public void setPauseButtonEnabled(boolean enabled) {
+        pauseButton.setEnabled(enabled);
+        resumeButton.setEnabled(!enabled);
+    }
+
+    public void pauseUpload() {
+        uploadTask.cancel(false);
+    }
+
+    public void resumeUpload() {
+        try {
+            TusUpload upload = new TusAndroidUpload(fileUri, this);
+            uploadTask = new UploadTask(this, client, upload);
+            uploadTask.execute(new Void[0]);
+        } catch (Exception e) {
+            showError(e);
+        }
+    }
+
+    private void setStatus(String text) {
+        status.setText(text);
+    }
+
+    private void setUploadProgress(int progress) {
+        progressBar.setProgress(progress);
+    }
+
+    private class UploadTask extends AsyncTask<Void, Long, URL> {
+        private FileTransferActivity activity;
+        private TusClient client;
+        private TusUpload upload;
+        private Exception exception;
+
+        public UploadTask(FileTransferActivity activity, TusClient client, TusUpload upload) {
+            this.activity = activity;
+            this.client = client;
+            this.upload = upload;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            activity.setStatus("Upload selected...");
+            activity.setPauseButtonEnabled(true);
+            activity.setUploadProgress(0);
+        }
+
+        @Override
+        protected void onPostExecute(URL uploadURL) {
+            activity.setStatus("Upload finished!\n" + uploadURL.toString());
+            activity.setPauseButtonEnabled(false);
+        }
+
+        @Override
+        protected void onCancelled() {
+            if(exception != null) {
+                activity.showError(exception);
+            }
+
+            activity.setPauseButtonEnabled(false);
+        }
+
+        @Override
+        protected void onProgressUpdate(Long... updates) {
+            long uploadedBytes = updates[0];
+            long totalBytes = updates[1];
+            activity.setStatus(String.format("Uploaded %d/%d.", uploadedBytes, totalBytes));
+            activity.setUploadProgress((int) ((double) uploadedBytes / totalBytes * 100));
+        }
+
+        @Override
+        protected URL doInBackground(Void... params) {
+            try {
+                TusUploader uploader = client.resumeOrCreateUpload(upload);
+                long totalBytes = upload.getSize();
+                long uploadedBytes = uploader.getOffset();
+
+                // Upload file in 1MiB chunks
+                uploader.setChunkSize(1024 * 1024);
+
+                while(!isCancelled() && uploader.uploadChunk() > 0) {
+                    uploadedBytes = uploader.getOffset();
+                    publishProgress(uploadedBytes, totalBytes);
+                }
+
+                uploader.finish();
+                return uploader.getUploadURL();
+
+            } catch(Exception e) {
+                exception = e;
+                cancel(true);
+            }
+            return null;
+        }
+    }
+
+    private void showError(Exception e) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Internal error");
+        builder.setMessage(e.getMessage());
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        e.printStackTrace();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.tus_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+
+        if (requestCode == REQUEST_FILE_SELECT) {
+            Uri uri = data.getData();
+            beginUpload(uri);
+        }
+    }
+}
